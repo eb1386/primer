de490740fae54c72bc80dfca32c60f5fa32e2edd
Evan Borodow <evan.borodow@icloud.com>
2026-03-17 10:51:43 -0700

    fix routing for static hosts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Learn from './pages/Learn';
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/home" element={<Landing />} />
         <Route path="/learn" element={<Learn />} />
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
